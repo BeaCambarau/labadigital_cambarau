606bd52b8c469b04a0bdbbf7e1094ac098860609
Beatrice <Beatrice@MacBook-Pro-di-Beatrice.local>
2017-04-28 16:00:05 +0200

    added static version

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function(){
+  // $('.carousel').slick({
+  //   carousel: 3
+  // });
+  $('.carousel').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+});
